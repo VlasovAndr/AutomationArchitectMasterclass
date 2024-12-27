32aeb9793548017de56052f46f6ea6922b9cc1e5
Andrey Vlasov <lion32211@yandex.ru>
2024-12-27 16:40:41 +0100

    Support remote web driver using Selenium Grid

--- src/test/java/com/automationframework/tests/AbstractTest.java
+++ src/test/java/com/automationframework/tests/AbstractTest.java
@@ -2,18 +2,46 @@ package com.automationframework.tests;
 
 import com.automationframework.pages.vendorportal.DashboardPage;
 import com.automationframework.pages.vendorportal.LoginPage;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public abstract class AbstractTest {
 
     protected WebDriver driver;
 
     @BeforeTest
-    public void setupDriver() {
-        this.driver = new ChromeDriver();
+    public void setupDriver() throws MalformedURLException {
+
+        if (Boolean.getBoolean("selenium.grid.enabled")) {
+            this.driver = getRemoteDriver();
+        } else {
+            this.driver = getLocalDriver();
+        }
+    }
+
+    private WebDriver getLocalDriver() {
+        return new ChromeDriver();
+    }
+
+    private WebDriver getRemoteDriver() throws MalformedURLException {
+        Capabilities capabilities;
+
+        if (System.getProperty("browser").equalsIgnoreCase("chrome")) {
+            capabilities = new ChromeOptions();
+        } else {
+            capabilities = new FirefoxOptions();
+        }
+
+        return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
     }
 
     @AfterTest
